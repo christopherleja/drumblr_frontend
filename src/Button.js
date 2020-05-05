005f71def79c7510618c8d198cf0ef29d55846dc
Chris Leja <christopher.leja@gmail.com>
2020-05-05 09:59:11 -0400

    added initial sequencing logic

--- src/Button.js
+++ src/Button.js
@@ -4,10 +4,11 @@ import MIDISounds from 'midi-sounds-react';
 class Button extends React.Component {
     
     // drum variable has to be in an array to work with library
-    handleClick = (sample, sampleIndex, beatIndex) => {
-        
-        this.props.togglePlaying(sampleIndex, beatIndex)
-        this.props.playDrum([sample])
+    handleClick = (sample, sampleIndex, beatIndex) => {  
+        let isMelodic = this.props.sampleObj.melody  
+        this.props.togglePlaying(isMelodic, sampleIndex, beatIndex)
+        this.props.sequenceThisNote(isMelodic, sample, sampleIndex, beatIndex)
+        // console.log(isMelodic, sampleIndex, beatIndex)
     }
 
 
@@ -16,7 +17,7 @@ class Button extends React.Component {
         let sampleIndex = this.props.sampleIndex
         let beatIndex = this.props.beatIndex
     return (         
-            <button className="SequenceBtn" onClick={() => {this.handleClick(sample, sampleIndex, beatIndex)}}>{this.props.sampleObj.name}</button>
+            <button className="SequenceBtn" onClick={() => {this.handleClick(sample, sampleIndex, beatIndex)}} >{this.props.sampleObj.name}</button>
     )
     }
 }
--- src/SampleContainer.js
+++ src/SampleContainer.js
@@ -4,8 +4,9 @@ import Button from '../src/Button'
 
 class SampleContainer extends React.Component{
     state = {
+        // just chose a default bpm to set state with
+        bpm: 120,
         // I just hardcoded in some basic drums to start
-        drums: [ 3, 17, 22, 26, 32, 35, 40, 45, 52, 58, 61, 70, 95, 105],
         drumObjs: [{
             id: 3, 
             name: 'Bass Drum', 
@@ -38,13 +39,54 @@ class SampleContainer extends React.Component{
             id: 45, 
             name: 'Pedal Hi-hat', 
             isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        }, {
+            id: 52, 
+            name: 'Mid Tom 2', 
+            isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        }, {
+            id: 56, 
+            name: 'Open Hi-hat', 
+            isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        }, {
+            id: 61, 
+            name: 'Mid Tom 1', 
+            isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        }, {
+            id: 70, 
+            name: 'Crash Cymbal', 
+            isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        }, {
+            id: 95, 
+            name: 'Tambourine', 
+            isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        }, {
+            id: 105, 
+            name: 'Cowbell', 
+            isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
         }
     ], instrumentObj: [{
             id: 458,
+            melody: true,
             name: "Violin",
             isPlaying: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
         }]
         
+    }
+
+    sequenceThisNote = (isMelodic, sample, sampleIndex, beatIndex) => {
+        let bpm = this.state.bpm 
+        let beat = 4 * 60 / bpm
+        let duration = beat/16
+        let time = this.midiSounds.contextTime()
+        let noteTime = time + (duration + (duration * beatIndex))
+        if (isMelodic){
+            this.midiSounds.playChordAt(noteTime, sample, [60], 0.5)
+            console.log(time, noteTime)
+        } else if (!isMelodic) {
+            this.midiSounds.playDrumsAt(noteTime, [sample])
+            console.log(time, noteTime)
+            console.log(sampleIndex, this.state.drumObjs[sampleIndex])
+        }
     }
 
     showDrums = () => {
@@ -52,26 +94,36 @@ class SampleContainer extends React.Component{
         this.midiSounds.player.loader.drumsKeys()
     }
 
-    togglePlaying = (drumIndex, beatIndex) => {
-        this.state.drumObjs[drumIndex].isPlaying[beatIndex] = !this.state.drumObjs[drumIndex].isPlaying[beatIndex]
+    togglePlaying = (melody, sampleIndex, beatIndex) => {
+        // first arg is boolean to determine whether sample is a drumObj or instrumentObj
+        // second arg is position in drum/instrumentObj array (for accessing correct sample)
+        // third arg is position in array, to access correct beat
+        if (melody){
 
-        console.log(this.state.drumObjs[drumIndex].isPlaying)
+            this.state.instrumentObj[sampleIndex].isPlaying[beatIndex] = !this.state.instrumentObj[sampleIndex].isPlaying[beatIndex]
+            console.log(this.state.instrumentObj[sampleIndex].isPlaying)
+        } else {
+            this.state.drumObjs[sampleIndex].isPlaying[beatIndex] = !this.state.drumObjs[sampleIndex].isPlaying[beatIndex]
+            console.log(this.state.drumObjs[sampleIndex].isPlaying)
+        }
     }
 
-    playDrum = (drum_id) => {
-        this.midiSounds.playDrumsNow([drum_id])
-    }
+    // playDrum = (drum_id) => {
+    //     this.midiSounds.playDrumsNow([drum_id])
+    //     console.log(this.midiSounds.contextTime())
+    // }
 
-    playInstrument = (instrument_id) => {
-        this.midiSounds.playChordNow(instrument_id, [60], 0.5);
-    }
+    // playInstrument = (instrument_id) => {
+    //     // first arg is instrument, second arg is pitch, third arg is note duration
+    //     this.midiSounds.playChordNow(instrument_id, [60], 0.5);
+    // }
 
     getDrumObjButtons = () => {
         return this.state.drumObjs.map((drumObj, index) => {
             let drumIndex = index
             let drum = drumObj
             return drumObj.isPlaying.map((beat, index) => {
-                return <Button key={drum.name + index} id={drum.id} sampleIndex={drumIndex} beatIndex={index} togglePlaying={this.togglePlaying} sampleObj={drum} playDrum={this.playDrum}/>
+                return <Button key={drum.name + index} id={drum.id} sampleIndex={drumIndex} beatIndex={index} togglePlaying={this.togglePlaying} sampleObj={drum} sequenceThisNote={this.sequenceThisNote}/>
             })
         })
     }
@@ -81,11 +133,36 @@ class SampleContainer extends React.Component{
             let instrumentIndex = index
             let instrument = instrumentObj
             return instrumentObj.isPlaying.map((beat, index) => {
-                return <Button key={instrument.name + index} id={instrument.id} sampleIndex={instrumentIndex} beatIndex={index} togglePlaying={this.togglePlaying} sampleObj={instrument} playDrum={this.playInstrument}/>
+                return <Button key={instrument.name + index} id={instrument.id} melody={instrument.melody} sampleIndex={instrumentIndex} beatIndex={index} togglePlaying={this.togglePlaying} sampleObj={instrument} sequenceThisNote={this.sequenceThisNote}/>
             })
         })
     }
 
+    // TODO: TOMORROW
+    // Wrap playing functionality in a single function
+    // Something like
+    // playBeat = (sample, sampleIndex, beatIndex) =>{
+    // let time = this.midiSounds.contextTime()
+    //  for (let i=0; i<17; i++)
+    //  let bpm = this.state.bpm 
+    //  let beat = 4 * 60 / bpm
+    //  let duration = beat/16
+    //  let noteTime = time + (duration + (duration * beatIndex))
+    //  if (isPlaying === true && isMelodic){
+    //  this.midiSounds.playChordAt(noteTime, sample, [60], 0.5)
+    //  } else if (isPlaying){
+    // this.midiSounds.playDrumsAt(noteTime, [sampleIndex])
+    // 
+    // } else { 
+    //  null
+    //  }
+    // if (i === 16){
+    // i = 0
+    // time = this.midiSounds.contextTime()
+    // }
+    // 
+    // }
+    
     render(){
         return (
             <div className="SampleContainer">
