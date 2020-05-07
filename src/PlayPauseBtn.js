a4d65dc19599e02909bb8bc6b3bbac24ae5c3c3a
Ryan Seit <ryan@ryanseit.com>
2020-05-07 16:18:33 -0400

    Updated PlayPauseBtn logic to toggle between play and stop and styling in NavBar.css

--- src/PlayPauseBtn.js
+++ src/PlayPauseBtn.js
@@ -2,15 +2,41 @@ import React from 'react'
 
 export default class PlayPauseBtn extends React.Component {
 
+  state = {
+    playActive: false,
+    stopActive: true
+}
+
+// when user click PlayBtn and state is false, set to true otherwise do nothing
+handlePlay = () => {
+  if (!this.state.playActive && this.state.stopActive) {
+    this.setState({
+      playActive: !this.state.playActive,
+      stopActive: !this.state.stopActive
+    })
+  }
+  this.props.playLoop();
+}
+
+// when user click StopBtn and state is false, set to true otherwise do nothing
+handleStop = () => {  
+  if (!this.state.stopActive && this.state.playActive) {
+    this.setState({
+      stopActive: !this.state.stopActive,
+      playActive: !this.state.playActive
+    })
+  }
+  this.props.stopLoop();
+}
 
   render() {
     return (
       <>
-        <div className="PlayBtn" onClick={this.props.playLoop}>
-          <p>Play</p>
+        <div className={this.state.playActive ? "PlayBtn active" : "PlayBtn inactive"} onClick={this.handlePlay}>
+          <p>▶</p>
         </div>
-        <div className="StopBtn" onClick={this.props.stopLoop}>
-          <p>Stop</p>
+        <div className={this.state.stopActive ? "StopBtn active" : "StopBtn inactive"} onClick={this.handleStop}>
+          <p>◼️</p>
         </div>
       </>
     )
