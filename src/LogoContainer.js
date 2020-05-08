71e61836ef0e91ca06538daaac5ed1c4220bea65
Ryan Seit <ryan@ryanseit.com>
2020-05-07 21:57:43 -0400

    HeaderContainer updated logo model

--- src/LogoContainer.js
+++ src/LogoContainer.js
@@ -8,6 +8,9 @@ export default class LogoContainer extends React.Component {
         <div className="DRUMBLRLogo">
           DRUMBLR
         </div>
+        <div className="ModelLogo">
+          <h1>DR-880</h1>
+        </div>
         <div className="MIDISoundsLogo">{this.props.midiSounds}</div>
       </div>
     )
